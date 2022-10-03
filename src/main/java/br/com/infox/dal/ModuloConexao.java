64427203d14af2362e5b18fd45d510bcfb6531af
Pedro Oliveira <pedro86gugalegal@gmail.com>
2022-10-03 08:56:22 -0300

    Projeto criado

---
+++ src/main/java/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.infox.dal;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Pedro Lucas
+ */
+public class ModuloConexao {
+    // Método responsável por estabelecer a conexão com o banco
+    public static Connection conector(){
+        java.sql.Connection conexao = null;
+        
+        // A linha abaixo "chama" o driver de conexão
+        String driver = "com.mysql.jdbc.Driver";
+        
+        // Armazenando informações referente ao banco
+        String url = "jdbc:mysql://localhost:3306/dbinfox";
+        String user = "root";
+        String password = "Pedro_465";
+        
+        // Estabelecendo a conexão com o banco
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
